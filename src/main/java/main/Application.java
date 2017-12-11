97469591ce02b21b75645f3bc408337dafad699a
Elisa Nurmi <elisa.nurmi@helsinki.fi>
2017-12-11 23:28:41 +0200

    merging ouzii's pull request, conflicts solved

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -20,9 +20,7 @@ public class Application {
     public Application(ItemHandler itemHandler, CommentHandler commentHandler) {
         this.scanner = new Scanner(System.in);
         this.itemHandler = itemHandler;
-
-      //pr seur 2 riviä 
-        this.ui = new UI(this.itemHandler, this.scanner);
+        this.ui = new UI(this.itemHandler, this.scanner, commentHandler);
         this.commentHandler = commentHandler;
     }
 
@@ -37,29 +35,11 @@ public class Application {
 
             request = request.toLowerCase();
 
-   // pr muutoksia:
-   //         if (request.equals("q") || request.equals("quit")) {
-   //             break;
-   //         } else {
-   //             this.ui.doCommand(request);
-
-            if (request.equals("s") || request.equals("save")) {
-                System.out.println("");
-                itemHandler.saveItem(scanner);
-            } else if (request.isEmpty() || request.equals("b") || request.equals("browse")) {
-                itemHandler.getItems(scanner);
-            } else if (request.equals("f") || (request.equals("filter"))) {
-                itemHandler.filterSearch(scanner);
-            } else if (request.equals("g") || request.equals("get")) {
-                itemHandler.findOne(scanner);
-            } else if (request.equals("q") || request.equals("quit")) {
-                System.out.println("Good bye!");
+            if (request.equals("q") || request.equals("quit")) {
                 break;
-            } else if (request.matches("\\d+")) {
-                if (itemHandler.getOne(Integer.parseInt(request)) != null)
-                    System.out.println(itemHandler.getOne(Integer.parseInt(request)) + "\n");
             } else {
-                System.out.println("Command not recognized, try using one of the above mentioned commands! (s, b, f, q)\n");
+                this.ui.doCommand(request);
+
             }
         }
     }
--- src/main/java/main/UI.java
+++ src/main/java/main/UI.java
@@ -1,17 +1,22 @@
 package main;
 
 import java.util.Scanner;
+
+import Comment.CommentHandler;
 import Item.ItemHandler;
 import java.sql.SQLException;
+
 
 public class UI {
 
     private ItemHandler itemHandler;
     private Scanner scanner;
+    private CommentHandler commentHandler;
 
-    public UI(ItemHandler itemHandler, Scanner scanner) {
+    public UI(ItemHandler itemHandler, Scanner scanner, CommentHandler commentHandler) {
         this.itemHandler = itemHandler;
         this.scanner = scanner;
+        this.commentHandler = commentHandler;
     }
 
     public void doCommand(String command) throws SQLException, ClassNotFoundException {
@@ -20,17 +25,18 @@ public class UI {
             itemHandler.saveItem(scanner);
         } else if (command.equals("browse") || command.isEmpty() || command.equals("b")) {
             itemHandler.getItems(scanner);
-        } else if (command.equals("f")) {
+        } else if (command.equals("f") || (command.equals("filter"))) {
+                itemHandler.filterSearch(scanner);
+        } else if (command.equals("g") || command.equals("get")) {
             itemHandler.findOne(scanner);
-        } else if (command.equals("add comment") || command.equals("a")) {
-            itemHandler.addComment(scanner);
         } else if (command.matches("\\d+")) {
             if (itemHandler.getOne(Integer.parseInt(command)) != null) {
                 System.out.println(itemHandler.getOne(Integer.parseInt(command)) + "\n");
             }
         } else {
-            System.out.println("Command not recognized, try using one of the above mentioned commands! \n");
+            System.out.println("Command not recognized, try using one of the mentioned commands above! \n");
         }
+
     }
 
 }
